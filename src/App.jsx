7f3e946a5874892c215e581c70191ca4345fefc9
leo-ngra <leonardo.ngra@gmail.com>
2022-12-28 16:03:35 -0300

    Postagens de noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header/Header"
+import ArticleList from "./components/ArticleList/ArticleList"
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <ArticleList />
+    </>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import ToggleTheme from '../ToggleTheme/ToggleTheme'
+
 const Header = () => {
     return(
-        <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+        <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
             <span className="text-gray-100">Olá, usuário</span>
             <h1  className="text-gray-100 text-xl">Newsletter</h1>
-            <input className="hidden sm:block" type="checkbox" />
+            <ToggleTheme />
         </div>
     )
 }
