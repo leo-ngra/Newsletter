5f312bb39e92c4121d4b5c82ed7ea713b7af95a7
leo-ngra <leonardo.ngra@gmail.com>
2023-01-03 14:41:11 -0300

    Otimização com Tailwind

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -5,12 +5,12 @@ const Article = ({ title, text, tags, image }) => {
         <h1 className="mb-2 text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h1>
         <div className="w-full pr-5 flex gap-2 justify-end">
             {
-                tags.map(tag => <span key={tag} className="px-4 py-1 bg-alura-100 dark:bg-dark-100 rounded-full text-gray-200 text-xs font-bold uppercase hover:bg-alura-200 hover:scale-110">{tag}</span>)
+                tags.map(tag => <span key={tag} className="alura-tag">{tag}</span>)
             }
         </div>
         <div className="flex flex-col gap-1">
             {
-                text.map((content, index) => <span key={index} className="text-alura-200 dark:text-gray-400">{content}</span>)
+                text.map((content, index) => <span key={index} className="text-alura-200 dark:text-gray-400 line-clamp-6 sm:line-clamp-none">{content}</span>)
             }
         </div>
         {image && <img className="sm:p-4" src={image} />}   
